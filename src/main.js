b1b1a5b8d415faeeb34b730d2ec91b3afccf72c2
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-04 15:14:49 -0300

    Lógica
    Criação das tags html no main.js para acionar função.

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ class App{
         //Form
         this.formulario = document.querySelector('form');
 
+        //Lista
+        this.lista = document.querySelector('.list-group');
+
         //Método para registrar os eventos do form
         this.registrarEventos();
     }
@@ -20,11 +23,65 @@ class App{
         evento.preventDefault();
 
         //Adicionar o repositorio a lista
-        this.repositorios.pish({
+        this.repositorios.push({
             nome: 'Nerd',
             descricao: 'Iconic font',
             avatar_url: 'https:',
             link: 'https://',
         })
+
+        //Renderizar a tela
+        this.renderizarTela();
+    }
+
+    renderizarTela(){
+        //Limpar o conteúdo de lista
+        this.lista.innerHTML = '';
+
+        //percorrer a lista de repositórios e criar elementos
+        this.repositorios.forEach(repositorio => {
+
+            //li
+            let li = document.createElement('li');
+            li.setAttribute('class', 'list-group-item list-group-item-action');
+
+            //<img>
+            let img = document.createElement('img');
+            img.setAttribute('src', repositorio.avatar_url);
+            li.appendChild(img);
+
+            //<strong>
+            let strong = document.createElement('strong');
+            let txtNome = document.createTextNode(repositorio.nome);
+            strong.appendChild(txtNome);
+            li.appendChild(strong);
+
+            //<p>
+            let p = document.createElement('p');
+            let txtDescricao = document.createTextNode(repositorio.descricao);
+            p.appendChild(txtDrescricao);
+            li.appendChild(p);
+
+            //<a>
+            let a = document.createElement('a');
+            a.setAttribute('target', '_blank');
+            a.setAttribute('href', repositorio.link);
+            let txtA = document.createTextNode('Acessar');
+            a.appendChild(txtA);
+            li.appendChild(a);
+
+
+            //adicionar <li></li> como filho da ul
+            this.lista.appendChild(li);
+
+            //limpar o conteudo input
+            this.formulario.querySelector('input[id=repositorio]').value = '';
+
+            //adicionar o foco no input
+            this.formulario.querySelector('input[id=repositorio]').focus();
+
+        });
     }
 }
+
+new App();
