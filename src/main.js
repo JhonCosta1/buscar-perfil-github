ba098343512e432613253e6c3c93996303afd412
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-04 14:05:14 -0300

    Início da lógica
    Iniciado o desenvolvimento da lógica da aplicação.
    
    mais.js;

--- src/main.js
+++ src/main.js
@@ -1,3 +1,30 @@
-//Início configuração arquivo main.js
+class App{
+    //Construtor
+    constructor(){
+        //Lista de repositórios
+        this.repositorios = [];
 
-document.querySelector('body').style.background = 'red';
+        //Form
+        this.formulario = document.querySelector('form');
+
+        //Método para registrar os eventos do form
+        this.registrarEventos();
+    }
+
+    registrarEventos(){
+        this.formulario.onsubmit = evento => this.adicionarRepositorio(evento);
+    }
+
+    adicionarRepositorio(evento){
+        //Evitar que o formulário recarregue a página
+        evento.preventDefault();
+
+        //Adicionar o repositorio a lista
+        this.repositorios.pish({
+            nome: 'Nerd',
+            descricao: 'Iconic font',
+            avatar_url: 'https:',
+            link: 'https://',
+        })
+    }
+}
